4d61e907157ae2999185c0635e88f42bc8af08bb
Lockmaster20 <diogoafernandes20@gmail.com>
2023-03-06 12:49:20 +0000

    Criadas as estruturas para os restantes dados e algumas alterações com estruturas anteriormente criadas.

--- EAD_26008/main.c
+++ EAD_26008/main.c
@@ -3,66 +3,63 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct registoPos {
-	char word1[19];
-	char word2[19];
-	char word3[19];
+typedef struct registoData {
+	int ano, mes, dia;
+} Data;
+
+typedef struct registoPosicao {
+	char palavra1[19], palavra2[19], palavra3[19];
 } Posicao;
 
-typedef struct registo {
-	int codigo;
-	char tipo[50];
+typedef struct registoContacto {
+	char tipoContacto[10];
+	char contacto[30];
+} Contacto;
+
+typedef struct registoEstado {
 	float bateria;
 	float autonomia;
-	Posicao posicao[1];			// posição do meio através de 3 palavras (what3words.com)
-	struct registo* seguinte;	// endereço de memória para uma strucr registo
+	//Posicao posicao;				// posição do meio através de 3 palavras (what3words.com)
+} Estado;
+
+typedef struct registoMeio {
+	int codigo;
+	char tipo[50];
+	//Estado estado;
+	struct registoMeio* seguinte;	// endereço de memória para uma struct
 } Meio;
+
+typedef struct registoUtilizador {
+	int codigo;
+	char login[20];					// Nome para iniciar sessão
+	char nome[50];					// Nome normal
+	char password[4];				// !!!! Tamanho temporário alterar depois de testes !!!!
+	float saldo;
+	Data dataNascimento;
+	int NIF;
+	char morada[50];
+	//Contacto contacto[5];			// máximo de 5 contactos por cliente
+	int gestor;
+	struct registoUtilizador* seguinte;
+} Utilizador;
+
+typedef struct registoAluguer {
+	int codigo;
+	int codigoUtilizador;
+	int codigoMeio;
+	//Data dataInicio, dataFim;
+	//Estado estadoInicial, estadoFinal;
+	int ativo;
+	float custo;
+	struct registoAluguer* seguinte;
+} Aluguer;
 
 main() {
 	//Fazer com listas ligadas porque ocupam menos espaço na memória, porque não é necessário dedicar memória ao definir a sruct
 	//cada "meio" regista o endereço do próximo "meio"
+
+	//Meio m;
+
+	//int a = m.codigo;
 	printf("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa\n");
-}
-
-/*
-
-Meio* inserirMeio(Meio* inicio, int cod, char tipo[], float bat, float aut);
-
-int listarMeios(Meio* inicio);
-
-int existeMeio(Meio* inicio, int cod);
-
-Meio* removerMeio(Meio* inicio, int cod);
-
-*/
-
-//Inserir novo registo
-Meio* inserirMeio(Meio* inicio, int cod, char tipo[], float bat, float aut) {
-	if (!existeMeio(inicio, cod)) {
-		Meio* novo = malloc(sizeof(struct registo));
-		if (novo != NULL) {
-			novo->codigo = cod;
-			strcpy(novo->tipo, tipo);
-			novo->bateria = bat;
-			novo->autonomia = aut;
-			novo->seguinte = inicio;
-			return(novo);
-		}
-	}
-	else return(inicio);
-}
-
-int existeMeio(Meio* inicio, int cod) { 
-	while (inicio != NULL) { 
-		if (inicio->codigo == cod) return(1); 
-		inicio = inicio->seguinte; 
-	} 
-	return(0);
-}
-
-void listarMeios(Meio* inicio){
-	while (inicio != NULL) {
-		printf("%d %s %.2f %.2f\n", inicio->codigo, inicio->tipo, inicio->bateria, inicio->autonomia);
-		inicio = inicio->seguinte;
-	}
 }
