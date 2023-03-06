b56b9db44403f2066a5eeecbbef04eea69ce756c
Lockmaster20 <diogoafernandes20@gmail.com>
2023-03-06 19:04:44 +0000

    Começo da programação de um login.

--- EAD_26008/main.c
+++ EAD_26008/main.c
@@ -1,4 +1,4 @@
-//#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -31,14 +31,14 @@ typedef struct registoMeio {
 
 typedef struct registoUtilizador {
 	int codigo;
-	char login[20];					// Nome para iniciar sessão
+	char utilizador[20];			// Nome para iniciar sessão
 	char nome[50];					// Nome normal
-	char password[4];				// !!!! Tamanho temporário alterar depois de testes !!!!
+	char password[4];				// !!!! Tamanho temporário alterar depois de testes !!!! Ex: "123" + \n
 	float saldo;
 	Data dataNascimento;
 	int NIF;
 	char morada[50];
-	//Contacto contacto[5];			// máximo de 5 contactos por cliente
+	//Contacto contacto[3];			// máximo de 3 contactos por cliente
 	int gestor;
 	struct registoUtilizador* seguinte;
 } Utilizador;
@@ -54,6 +54,53 @@ typedef struct registoAluguer {
 	struct registoAluguer* seguinte;
 } Aluguer;
 
+int login() {
+	char utilizador[20], password[4];
+
+	printf("Utilizador:\n");
+	scanf("%[^\n]s", utilizador);
+	scanf("%*c");					// !!! Se possível ver outra solução para não passar à frente !!!
+	printf("Password:\n");
+	scanf("%[^\n]s", password);
+	scanf("%*c"); 
+	
+	FILE* fp;
+	int fcodigo;
+	char futilizador[20];
+	char fnome[50];
+	char fpassword[4];
+	float fsaldo;
+	Data fdataNascimento;
+	int fNIF;
+	char fmorada[50];
+	int fgestor;
+
+	fp = fopen("dados/utilizadores.txt", "r");
+	if (fp != NULL)
+	{
+		while (!feof(fp))
+		{
+			int check = fscanf(fp, "%d;%[^;];%[^;];%[^;];%f;%d-%d-%d;%d;%[^;];%d\n", &fcodigo, futilizador, fnome, fpassword, &fsaldo, &fdataNascimento.ano, &fdataNascimento.mes, &fdataNascimento.dia, &fNIF, fmorada, &fgestor);
+			if (check != 11) {
+				printf("\n\n\n////////////////// ERRO //////////////////\n\n\n");
+				return 0;
+			}
+
+			int a = 0;
+		}
+		fclose(fp);
+	}
+
+	if (!strcmp(utilizador, futilizador) && !strcmp(password, fpassword))
+	{
+		return 1;
+	}
+
+	int a = 0;
+
+	return 0;
+}
+
 main() {
 	//Fazer com listas ligadas porque ocupam menos espaço na memória, porque não é necessário dedicar memória ao definir a sruct
 	//cada "meio" regista o endereço do próximo "meio"
@@ -61,5 +108,5 @@ main() {
 	//Meio m;
 
 	//int a = m.codigo;
-	printf("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa\n");
+	printf("\n\nLogin? %d\n", login());
 }
